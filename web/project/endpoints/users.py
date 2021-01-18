7c4fc5648ea5099d82398f1f6cb04f1a20b11e4a
XantisS0 <patryk.makowski23@gmail.com>
2021-01-18 15:16:45 +0100

    Repair patch and delete

--- web/project/endpoints/users.py
+++ web/project/endpoints/users.py
@@ -6,14 +6,6 @@ from ..models.server import Server
 
 from ..models.user import User
 
-user_resource_fields = {
-    'id': fields.Integer,
-    'server_id': fields.Integer,
-    # 'server': fields.String,
-    'username': fields.String,
-    'ranks': fields.List
-}
-
 user_post_args = reqparse.RequestParser()
 user_post_args.add_argument(
     "username",
@@ -30,7 +22,7 @@ user_post_args.add_argument(
 user_update_args = reqparse.RequestParser()
 user_update_args.add_argument("server_id", type=int)
 user_update_args.add_argument("username", type=str)
-user_update_args.add_argument("ranks", type=list)
+user_update_args.add_argument("ranks", type=list, location='json')
 
 
 class UsersEndpoint(Resource):
@@ -72,30 +64,52 @@ class UsersEndpoint(Resource):
 
 
 class UserEndpoint(Resource):
-    @marshal_with(user_resource_fields)
     def patch(self, serv_id, user_id):
         args = user_update_args.parse_args()
 
-        result = User.query.filter_by(
-            id=user_id, server_id=serv_id).first()
+        user = User.query.filter_by(id=user_id).first()
 
-        if not result:
-            abort(404, message="User with this id does not exist.")
+        if not user:
+            abort(404, message="User with this id does not exist on this server.")
 
-        for arg in args:
-            if arg:
-                result.arg = arg
+        if args['username']:
+            user.username = args['username']
+        if args['ranks']:
+            ranks = Rank.query.filter_by(user_id=user_id).all()
+            ranks_array = [rank.name for rank in ranks]
+            if set(args['ranks']) != set(ranks_array):
+                db.session.delete(ranks)
+                for rank in args['ranks']:
+                    item = Rank(name=rank, user=user)
+                    db.session.add(item)
+        if args['server_id']:
+            user.server_id = args['server_id']
 
         db.session.commit()
-        return result, 200
+
+        user = User.query.filter_by(id=user_id).first()
+
+        ranks = Rank.query.filter_by(user_id=user_id).all()
+        ranks_array = [rank.name for rank in ranks]
+
+        response = {
+            "id": user.id,
+            "server_id": user.server_id,
+            "username": user.username,
+            "ranks": ranks_array
+        }
+
+        return response, 200
 
     def delete(self, serv_id, user_id):
-        result = User.query.filter_by(id=user_id, server_id=serv_id)
+        result = User.query.filter_by(id=user_id).filter_by(server_id=serv_id).first()
+        Rank.query.filter_by(user_id=user_id).delete()
 
         if not result:
             abort(404, message="User with this id does not exist.")
 
         db.session.delete(result)
+        db.session.commit()
         return "", 200
 
 
